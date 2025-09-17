c6e03a30cea12011a27b76144e315c5a3d9aa41c
Bryce Baker <bryce.baker@ucdenver.edu>
2025-09-16 21:48:44 -0600

    added suspense

--- auto_pd_exam/src/app/signin/page.js
+++ auto_pd_exam/src/app/signin/page.js
@@ -1,13 +1,14 @@
 import SignIn from '../components/SignIn';
 import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { Suspense } from 'react';
 
 export default function SignInPage() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
             <NavBar></NavBar>
             <div className='row-start-2 fade-in'>
-                <SignIn></SignIn>
+                <Suspense><SignIn></SignIn></Suspense>
             </div>
             <Footer></Footer>
         </div>
